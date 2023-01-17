65ab05a30fbd3d8610715f61d1732f9c728204a9
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-01-17 23:20:24 +0100

    add 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,16 @@
+const background = document.querySelector(".widget");
+const codeColor = document.querySelector(".color");
+const btnChangeColor = document.querySelector(".change-color");
+
+btnChangeColor.addEventListener("click", handleClickChange);
+
+function handleClickChange(event) {
+  codeColor.textContent = getRandomHexColor();
+  background.style.backgroundColor = getRandomHexColor();
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
