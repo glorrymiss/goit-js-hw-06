582d64529076533adab284ae618af18915c1e3f5
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-01-20 15:00:25 +0100

    corect EX 3

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -7,11 +7,15 @@ input.addEventListener("blur", handleTargetInputTextControl);
 
 function handleTargetInputTextControl(event) {
   console.log(event);
-  console.log(event.currentTarget.value);
-  console.log(event.currentTarget.value.length);
-  if (event.currentTarget.value.length === inputLength) {
+
+  const { currentTarget } = event;
+  const { value } = currentTarget;
+  console.log(value);
+  input.classList.remove("valid");
+  input.classList.remove("invalid");
+  if (value.trim().length === inputLength) {
     input.classList.add("valid");
-  } else if (event.currentTarget.value.length !== inputLength) {
+  } else {
     input.classList.add("invalid");
   }
 }
