2668bed9794434a67d67cbcaf28b280d35f94bcd
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-01-20 23:41:03 +0100

    add 10 - work

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,29 +1,30 @@
 const input = document.querySelector(".input");
+
 const btnCreate = document.querySelector("button[data-create]");
 const btnRemove = document.querySelector("button[data-destroy]");
 const box = document.querySelector("#boxes");
+let amount;
+input.addEventListener("input", (event) => {
+  amount = event.currentTarget.value;
+  console.log(amount);
+});
 
 btnCreate.addEventListener("click", creatBoxes);
 
 btnRemove.addEventListener("click", destroyBoxes);
 
-function creatBoxes(amount) {
-  if (Number(amount) > 30) {
-    const newDiv = document.body.create("<div>");
-    newDiv.style.width = `${amount}px`;
-    newDiv.style.height = `${amount}px`;
+function creatBoxes(event) {
+  for (let i = 0; i < amount; i += 1) {
+    const newDiv = document.createElement("div");
+    newDiv.style.width = 30 + 10 * i + "px";
+    newDiv.style.height = 30 + 10 * i + "px";
     newDiv.style.backgroundColor = getRandomHexColor();
     box.append(newDiv);
-    return;
-  } else if (Number(amount) <= 30) {
-    alert("Число має бути більшим за 30");
   }
 }
-
 function destroyBoxes() {
-  box.currentTarget.reset();
+  box.innerHTML = "";
 }
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
